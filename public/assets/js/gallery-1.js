bd69db2314421bfe2f3371eac8ef0bf3e6127481
Alexei Kopylov <akopylov@aligntech.com>
2015-04-10 18:16:07 +0300

    service added to directive

--- public/assets/js/gallery-1.js
+++ public/assets/js/gallery-1.js
@@ -1,9 +1,26 @@
 angular
     .module("Gallery", [])
     .controller('GalleryController', ['$scope', function($scope){
-        $scope.showImage = '*';
+        $scope.message = $scope.latitude = $scope.longitude = '';
     }])
-    .directive('galleryItem', function(){
+    .factory('getLocation', ['$http', function($http) {
+        return function(name, scope, element) {
+            var show = function(text) {
+              element.find('div').text(text);
+            };
+            $http.get('/image/' + name)
+                .success(function(data, status, headers, config) {
+                    console.log('Latitude:' + data.latitude + '; Longitude:' + data.longitude);
+                    show(data.latitude + '/' + data.longitude);
+                })
+                .error(function(data, status, headers, config) {
+                    console.log('Error: ' + status);
+                    show('No GPS data');
+                });
+        }
+    }])
+    .directive('galleryItem', function(getLocation){
+        var isMapVisible = false;
         return {
             restrict: 'A'
              , link: function(scope, element, attr) {
@@ -13,6 +30,7 @@ angular
                     console.log('Clicked: ' + name);
                     element.find('img').toggleClass('ng-hide');
                     element.find('div').toggleClass('ng-hide');
+                    if (isMapVisible = !isMapVisible) getLocation(name, scope, element);
                 });
             }
         }
