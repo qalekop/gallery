872fb56b6df6235ad7a9eb748a4d3ab2e2db3f3f
alexeikopylov <alexei.kopylov.sr@gmail.com>
2015-04-20 14:44:17 +0300

    playing with redis

--- public/assets/js/gallery.js
+++ public/assets/js/gallery.js
@@ -1,23 +1,51 @@
-// gallery angular app
-
-var galleryApp = angular.module("Gallery", []);
-
-galleryApp.controller('GalleryController', ['$scope', 'getLocation', function($scope, getLocation) {
-	$scope.name = 'No image selected';
-    $scope.imageName = function(name) {
-		getLocation(name, $scope);
-    };
-}])
-.factory('getLocation', ['$http', function($http) {
-	return function(name, scope) {
-	$http.get('/image/' + name)
-		.success(function(data, status, headers, config) {
-			console.log('Latitude:' + data.latitude + '; Longitude:' + data.longitude);
-			scope.name = 'Latitude:' + data.latitude + '; Longitude:' + data.longitude;
-		})
-		.error(function(data, status, headers, config) {
-			console.log('Error: ' + status);
-			scope.name = 'No GPS data';
-		});
-	}
-}]);
+angular
+    .module("Gallery", [])
+    .controller('GalleryController', ['$scope', function($scope){
+        $scope.message = $scope.latitude = $scope.longitude = '';
+    }])
+    .factory('getLocation', ['$http', function($http) {
+        return function(name, scope, element) {
+            var show = function(text) {
+              element.find('div').text(text);
+            };
+            var openMap = function(latitude, longitude) {
+                var latLng = new google.maps.LatLng(latitude, longitude);
+                var m = new google.maps.Map(element.find('div')[0], {
+                    zoom: 8,
+                    center: latLng,
+                    mapTypeId: google.maps.MapTypeId.ROADMAP
+                });
+                new google.maps.Marker({
+                    map: m,
+                    position: latLng,
+                    clickable: false
+                });
+            };
+            $http.get('/image/' + name)
+                .success(function(data, status, headers, config) {
+                    console.log('Latitude:' + data.latitude + '; Longitude:' + data.longitude);
+                    openMap(data.latitude, data.longitude);
+                })
+                .error(function(data, status, headers, config) {
+                    console.log('Error: ' + status);
+                    show('No GPS data');
+                });
+        }
+    }])
+    .directive('galleryItem', ['getLocation', function(getLocation){
+        var isMapVisible = false;
+        return {
+            restrict: 'A'
+             , link: function(scope, element, attr) {
+                var name = attr.galleryItem;
+                console.log('link  for ' + name);
+                element.on('click', function() {
+                    console.log('Clicked: ' + name);
+                    element.find('img').toggleClass('ng-hide');
+                    element.find('div').toggleClass('ng-hide');
+                    if (isMapVisible = !isMapVisible) getLocation(name, scope, element);
+                });
+            }
+        }
+    }])
+;
---
+++ server-redis.js
@@ -0,0 +1,114 @@
+var express = require('express')
+  , fs = require('fs')
+  , parser = require('exif-parser')
+  , gm = require('googlemaps')
+  , async = require('async')
+  , stylus = require('stylus')
+  , nib = require('nib')
+  , redis = require('redis').createClient()
+  ;
+
+var app = express();
+function compile(str, path) {
+  return stylus(str).set('filename', path).use(nib());
+};
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(stylus.middleware(
+  { src: __dirname + '/public/'
+    , compile: compile
+  }
+));
+app.use(express.static(__dirname + '/public'));
+
+/////////////////////////////
+var images = [];
+var fnPrefix = __dirname + '/public/assets/gallery';
+var files = fs.readdirSync(fnPrefix);
+var file
+    , exif
+    , coords
+    , width
+    , height;
+
+files.forEach(function(file, i) {
+  exif = parser.create(fs.readFileSync(fnPrefix + '/' + file)).parse();
+  height = exif.imageSize.height;
+  width = exif.imageSize.width;
+  var image = {'name': file, 'endroit': 'Quelque part', 'width': width, 'height': height};
+  images[i] = image;
+  if (exif.tags.GPSVersionID) {
+    image.waitForName = true;
+    image.latitude = exif.tags.GPSLatitude; image.longitude = exif.tags.GPSLongitude;
+  }
+});
+
+async.each(images
+  , function(item, callback) {
+    if (item.waitForName) {
+      gm.reverseGeocode(item.latitude + ',' + item.longitude, function(err, data){
+        redis.hmset(item.name, {
+          latitude: item.latitude
+          , longitude: item.longitude
+          , endroit: data.results[2].formatted_address}, function(err, obj) {callback();}
+        );
+      });
+    } else {
+      callback();
+    }
+  }
+  , function(err) {
+    console.log('over');
+    app.listen(3000, function() {console.log('listening...');});
+  }
+);
+
+/////////////////////////////
+app.get('/', function (req, res) {
+  res.render('index', { title: 'Gallery', images: images });
+});
+
+app.get('/image/:id', function(req, res) {
+  var id = req.params.id;
+  console.log('*** request for ' + id);
+  redis.exists(id, function(err, reply) {
+    if (reply == 1) {
+      async.parallel({
+        latitude: function(callback) {
+          redis.hexists(id, 'latitude', function(err, reply) {
+            if (reply == 1) {
+              redis.hget(id, 'latitude', function (err, obj) {
+                callback(err, obj);
+              });
+            } else {
+              callback('Not found', null);
+            }
+          });
+        }
+        , longitude: function(callback) {
+          redis.hexists(id, 'longitude', function(err, reply) {
+            if (reply == 1) {
+              redis.hget(id, 'longitude', function (err, obj) {
+                callback(err, obj);
+              });
+            } else {
+              callback('Not found', null);
+            }
+          });
+         }
+      }, function(err, result){
+        if (!err && result.latitude && result.longitude) {
+          console.log('Found: ' + result.latitude + '/' + result.longitude);
+          res.json({latitude: result.latitude, longitude: result.longitude});
+        } else {
+          console.log('Not found');
+          res.status(404).json({error: 'Not found'});
+        }
+      });
+    } else {
+      console.log('Not found');
+      res.status(404).json({error: 'Not found'});
+    }
+  });
+});
